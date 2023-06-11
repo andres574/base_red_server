7639939502708c6b55b1ea4d80440f238a4d41d7
Andres Perez <perezandres574@gmail.com>
2023-06-10 22:28:11 -0500

    control npm

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ class Server {
 
     constructor() {
         this.app = express();
-       this.PORT = process.env.PORT;
+       this.PORT = process.env.PORT; 
        this.usuariosPath = '/api/usuarios';
 
 
